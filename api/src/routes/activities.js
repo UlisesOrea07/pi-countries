60df32e5ff15ac924867cbd5154a4d8bc4cdd7df
ulises <j.ulisesorea@gmail.com>
2022-03-29 21:52:58 -0600

    backend added routes

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,4 +1,4 @@
-const { Activity } = require('../db');
+const { Activity, Country } = require('../db');
 const router = require('express').Router();
 
 router.get('/activities', async (req, res) => {
@@ -8,8 +8,23 @@ router.get('/activities', async (req, res) => {
     } catch (error) {
         res.send(error);
     }
-
 });
+
+router.get('/activities/filter', async (req, res) => {
+    try {
+        const { name } = req.query;
+        const activities = await Activity.findAll({
+            where: {
+                name: name
+            },
+            include: Country
+        });
+        res.json({ "status": "ok", "data": activities, "message": "Response succefuly" })
+    } catch (error) {
+        res.send({ status: "error", error: "Something went wrong" + error });
+    }
+});
+
 router.post('/activity', async (req, res) => {
     const { name, difficulty, duration, season, countries } = req.body;
     if (!name, !difficulty, !duration, !season) {
--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -16,13 +16,15 @@ const { Op } = require('sequelize');
 
 router.get('/countries', async (req, res) => {
     try {
-        const { expresion } = req.query;
+        let { expresion } = req.query;
         let countries = [];
         if (!expresion) {
             countries = await Country.findAll({
                 include: Activity,
             });
         } else {
+            expresion = expresion.toLowerCase();
+            expresion = expresion.charAt(0).toUpperCase() + expresion.slice(1);
             countries = await Country.findAll({
                 where: {
                     name: {
@@ -52,6 +54,21 @@ router.get('/country/:id', async (req, res) => {
         });
         country ? res.json({ "status": "ok", "data": country, "message": "Response succefuly" })
             : res.json({ status: "not", error: "Not found" });
+    } catch (error) {
+        res.send({ status: "error", error: "Something went wrong" + error });
+    }
+});
+
+router.get('/countries/filter', async (req, res) => {
+    try {
+        const { continent } = req.query;
+        const countries = await Country.findAll({
+            where: {
+                continent: continent
+            },
+            include: Activity
+        });
+        res.json({ "status": "ok", "data": countries, "message": "Response succefuly" })
     } catch (error) {
         res.send({ status: "error", error: "Something went wrong" + error });
     }
